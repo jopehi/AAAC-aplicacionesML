a93e60ac960bd804736f5fcdb043cc0d250509b0
hernando.pena <hernando.pena@unad.edu.co>
2026-04-22 14:04:19 -0500

    app demo fintech

---
+++ fecha/fecha.py
@@ -0,0 +1,6 @@
+import time
+
+# Obtener la fecha y hora actual en formato Unix timestamp
+timestamp = int(time.time())
+
+print("Fecha y hora actual en formato Unix timestamp:", timestamp)
---
+++ fecha/verFecha.php
@@ -0,0 +1,8 @@
+<?php
+$timestamp = 1712595243; // Esto es un ejemplo, reemplázalo con tu timestamp real
+
+$fecha_y_hora = date('Y-m-d H:i:s', $timestamp);
+
+echo "Fecha timestamp: $timestamp<br>";
+echo "Fecha limpia: $fecha_y_hora<br>";
+?>
